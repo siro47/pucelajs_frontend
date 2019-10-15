bc6f6df6f97c631465e692fbd12e5de08e64e4d3
sramirez <sramirez@solidgeargroup.com>
2019-10-15 10:55:50 +0200

    Base sockets connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Socket} from "ngx-socket-io";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
+
+    constructor(private socket: Socket) { }
+
+    ngOnInit() {
+        this.socket.fromEvent('premMessage')
+            .subscribe(msg => console.log(msg))
+    }
 }
